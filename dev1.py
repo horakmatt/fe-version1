b759dda63c903efad0d3b37fccebde319fb66cd1
Matthew Horak <horakmatt@gmail.com>
2025-09-06 22:54:37 -0700

    Wording chnages

--- dev1.py
+++ dev1.py
@@ -85,9 +85,9 @@ if uploaded_file is not None:
     n_decline = len(df_res[df_res['decision'] == 'Decline'])
 
     st.markdown(f"""We successflly processed {len(df_res)} applicants with the following decision distribution.\n
-    * {n_accept} 'Accept' decisions\n
-    * {n_review} 'Review' decisions\n
-    * {n_decline} 'Decline' decisions\n""")
+    - {n_accept} 'Accept' decisions
+    - {n_review} 'Review' decisions
+    - {n_decline} 'Decline' decisions""")
 
     st.write("Full summaries are available below.")
 
